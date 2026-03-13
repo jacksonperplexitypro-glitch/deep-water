ada83f3443aaf2ac6bf0541a71eadb3563c58486
ct-jacksonchin <164849641+ct-jackonchin@users.noreply.github.com>
2026-03-13 09:21:57 +0800

    123

--- index.php
+++ index.php
@@ -251,7 +251,7 @@ function gtag_report_conversion(url) {
             <div class="container hero-content">
                 <div class="hero-text">
                     <h1 class="hero-title">DEEP WATER SOUTH</h1>
-                    <h2 class="hero-subtitle">第6B期 GRANDE BLANC</h2>
+                    <h2 class="hero-subtitle">本網站非發展商官方網站，僅提供資訊參考</h2>
                     <p class="hero-description-1">全新臨海高端超級豪宅</p>
                     <p class="hero-description-2">香港仔黃竹坑站港島南岸</p>
                     
